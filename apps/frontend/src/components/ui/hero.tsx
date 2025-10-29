a77686c71b3056776aa46fc0594de865efebef3c
nikita <nigork@taltech.ee>
2025-10-29 21:48:31 +0200

    feat(ui): add InputGroupIcon and refactor Hero inputs/buttons

--- apps/frontend/src/components/ui/hero.tsx
+++ apps/frontend/src/components/ui/hero.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input"
 import { MagnifyingGlassIcon, MapPinIcon, BriefcaseIcon } from "@heroicons/react/24/outline";
+import { InputGroupIcon } from "@/components/ui/input-group"
 
 export default function Hero() {
   return (
@@ -11,25 +13,16 @@ export default function Hero() {
       <div className="mt-[20px] shadow-blue bg-white rounded-2xl shadow-md flex flex-col items-center w-full max-w-3xl pb-[44px]">
         <div className="flex flex-col px-[46px] pt-[44px] sm:flex-row w-full gap-[20px]">
           <div className="relative flex-2">
-            <BriefcaseIcon className="absolute left-[10px] top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
-            <input
-              type="text"
-              placeholder="Job title, keywords or company"
-              className="w-full rounded-full border border-gray-300 pl-[36px] pr-4 py-3 text-gray-700 focus:outline-none focus:ring-2 focus:ring-[#2563EB]"
-            />
+            <InputGroupIcon as={BriefcaseIcon} />
+            <Input placeholder="Job title, keywords or company" />
           </div>
           <div className="relative flex-1">
-            <MapPinIcon className="absolute left-[10px] top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400"/>
-            <input
-            type="text"
-            placeholder="Location"
-            className="flex-1 rounded-full border border-gray-300 pl-[36px] pr-4 py-3 text-gray-700 focus:outline-none focus:ring-2 focus:ring-[#2563EB]"
-          />
+            <InputGroupIcon as={MapPinIcon} />
+            <Input placeholder="Location" />
           </div>
         </div>
-          
-          <Button variant="search" size='none' className="relative pl-[34px] pr-[22px] w-full sm:w-auto mt-[17px]">
-            <MagnifyingGlassIcon className="absolute left-[10px] top-1/2 -translate-y-1/2 h-5 w-5 text-white" />
+          <Button variant="search" size='none' className="relative mt-[17px]">
+            <InputGroupIcon as={MagnifyingGlassIcon} className="text-white"/>
             Search Jobs
           </Button>
       </div>
