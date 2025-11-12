d03d258705711670fab3fe1689a6da703e1d8074
Kurb Ram <kurbram@Kurbs-MacBook-Pro-2.local>
2025-11-12 12:05:59 +0200

    format

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -3,16 +3,20 @@ import { AuthService } from "./auth.service";
 import { LocalAuthGuard } from "./local-auth.guard";
 import { JwtAuthGuard } from "./jwt-auth.guard";
 
+
+// localhost:3001/api/auth
 @Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  // localhost:3001/api/auth/login
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
     return this.authService.login(req.user);
   }
 
+  // localhost:3001/api/auth/profile
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
--- apps/frontend/src/components/ui/input.tsx
+++ apps/frontend/src/components/ui/input.tsx
@@ -1,34 +1,28 @@
+"use client";
 
-"use client"
-
-import { cva, type VariantProps } from "class-variance-authority"
-import { cn } from "@/lib/utils"
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
 
 const inputVariants = cva(
   "w-full rounded-full border border-gray-300 pl-[36px] pr-4 py-3 text-gray-700 focus:outline-none focus:ring-2 focus:ring-[#2563EB]",
   {
     variants: {
       variant: {
-        default: "", 
-      },
+        default: ""
+      }
     },
     defaultVariants: {
-      variant: "default",
-    },
+      variant: "default"
+    }
   }
-)
+);
 
 function Input({
   className,
   variant,
   ...props
 }: React.ComponentProps<"input"> & VariantProps<typeof inputVariants>) {
-  return (
-    <input
-      className={cn(inputVariants({ variant, className }))}
-      {...props}
-    />
-  )
+  return <input className={cn(inputVariants({ variant, className }))} {...props} />;
 }
 
-export { Input, inputVariants }
+export { Input, inputVariants };
